eecca31f5c8b6f0635cfdf3d5eda90679764714e
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2024-01-15 15:25:10 +0400

    Initial Commit

---
+++ FastingTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FastingTimer
+//
+//  Created by Tako Menteshashvili on 15.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Tako Menteshashvili on 15.01.24.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
